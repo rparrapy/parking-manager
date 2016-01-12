f497ce09bc376122ee082f0ada12802862112c09
mattiasarro <mattias.arro@gmail.com>
2016-01-12 20:56:28 +0100

    hacky jQuery dateselect
    we’re running an old version of angular, can’t do it angular+bootstrap
    style

--- src/main/resources/webapp/js/parking-spot-controllers.js
+++ src/main/resources/webapp/js/parking-spot-controllers.js
@@ -99,6 +99,11 @@ parkingSpotControllers.controller('ParkingSpotDetailCtrl', [
         // update navbar
         angular.element("#navbar").children().removeClass('active');
         angular.element("#parking-spot-navlink").addClass('active');
+        $( "#datepicker" ).datepicker({
+            onSelect: function( selectedDate ) {
+                console.log(selectedDate);
+            }
+        });
     
         // free resource when controller is destroyed
         $scope.$on('$destroy', function(){
@@ -125,94 +130,5 @@ parkingSpotControllers.controller('ParkingSpotDetailCtrl', [
                 parkingSpotServices.assignAttr($scope.parkingSpot);
                 parkingSpotServices.startObserving($scope.parkingSpot);
             });
-    
-            // listen for parkingSpots registration/deregistration/observe
-            $scope.eventsource = new EventSource('http://localhost:8080/event?ep=' + $routeParams.parkingSpotId);
-    
-            var registerCallback = function(msg) {
-                $scope.$apply(function() {
-                    $scope.deregistered = false;
-                    $scope.parkingSpot = JSON.parse(msg.data);
-                    lwResources.buildResourceTree($scope.parkingSpot.rootPath, $scope.parkingSpot.objectLinks, function (objects){
-                        $scope.objects = objects;
-                    });
-                });
-            }
-            $scope.eventsource.addEventListener('REGISTRATION', registerCallback, false);
-    
-            var deregisterCallback = function(msg) {
-                $scope.$apply(function() {
-                    $scope.deregistered = true;
-                    $scope.parkingSpot = null;
-                });
-            }
-            $scope.eventsource.addEventListener('DEREGISTRATION', deregisterCallback, false);
-    
-            var notificationCallback = function(msg) {
-                $scope.$apply(function() {
-                    var content = JSON.parse(msg.data);
-                    var resource = lwResources.findResource($scope.objects, content.res);
-                    if (resource) {
-                        if("value" in content.val) {
-                            // single value
-                            resource.value = content.val.value
-                        }
-                        else if("values" in content.val) {
-                            // multiple instances
-                            var tab = new Array();
-                            for (var i in content.val.values) {
-                                tab.push(i+"="+content.val.values[i])
-                            }
-                            resource.value = tab.join(", ");
-                        }
-                        resource.valuesupposed = false;
-                        resource.observed = true;
-    
-                        var formattedDate = $filter('date')(new Date(), 'HH:mm:ss.sss');
-                        resource.tooltip = formattedDate;
-                    } else {
-                        // instance?
-                        var instance = lwResources.findInstance($scope.objects, content.res);
-                        if (instance) {
-                            instance.observed = true;
-                            for(var i in content.val.resources) {
-                                var tlvresource = content.val.resources[i];
-                                resource = lwResources.addResource(instance.parent, instance, tlvresource.id, null)
-                                if("value" in tlvresource) {
-                                    // single value
-                                    resource.value = tlvresource.value
-                                } else if("values" in tlvresource) {
-                                    // multiple instances
-                                    var tab = new Array();
-                                    for (var j in tlvresource.values) {
-                                        tab.push(j+"="+tlvresource.values[j])
-                                    }
-                                    resource.value = tab.join(", ");
-                                }
-                                resource.valuesupposed = false;
-                                resource.tooltip = formattedDate;
-                            }
-                        }
-                    } // TODO object level
-                });
-            }
-            $scope.eventsource.addEventListener('NOTIFICATION', notificationCallback, false);
-    
-            $scope.coaplogs = [];
-            var coapLogCallback = function(msg) {
-                $scope.$apply(function() {
-                    var log = JSON.parse(msg.data);
-                    log.date = $filter('date')(new Date(log.timestamp), 'HH:mm:ss.sss');
-                    console.log(log);
-                    $scope.coaplogs.push(log);
-                });
-            }
-            $scope.eventsource.addEventListener('COAPLOG', coapLogCallback, false);
-    
-            // coap logs hidden by default
-            $scope.coapLogsCollapsed = true;
-            $scope.toggleCoapLogs = function() {
-                $scope.coapLogsCollapsed = !$scope.coapLogsCollapsed;
-            }
         });
 }]);
