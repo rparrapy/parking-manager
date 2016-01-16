68e4713fc9573acb42eb2f6ea0dca41fe30415d9
mattiasarro <mattias.arro@gmail.com>
2016-01-16 16:36:35 +0100

    downgrade mongo driver

--- src/main/java/nl/tue/iot/reservation/BillsServlet.java
+++ src/main/java/nl/tue/iot/reservation/BillsServlet.java
@@ -1,4 +1,9 @@
 package nl.tue.iot.reservation;
+import com.mongodb.*;
+import com.mongodb.client.FindIterable;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.eclipse.leshan.server.LwM2mServer;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
@@ -7,13 +12,24 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.mongodb.client.model.Filters.*;
 
 public class BillsServlet extends HttpServlet {
     private final LwM2mServer server;
+    public static MongoDatabase db;
+    public static MongoClient mongoClient;
 
     public BillsServlet(LwM2mServer server) {
         this.server = server;
+        this.mongoClient = new MongoClient("localhost", 27017);
+        this.db = mongoClient.getDatabase("test");
     }
+
+
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -21,13 +37,53 @@ public class BillsServlet extends HttpServlet {
         String date = req.getParameter("date"); // format: ISO-8601 yyyy-mm-dd
         String path;
 
-        if (parkingSpotId == null) {
-            path = "all-parking-spot-bills.json";
-        } else {
-            path = "parking-spot-bills.json";
+        MongoCollection<Document> coll = db.getCollection("reservations");
+        try {
+            SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss.SSS\'Z\'");
+            FindIterable cursor = coll.find(and(gte("date", df.parse(date + "T00:00:00.000Z")), lte("date", df.parse(date + "T23:59:59.999Z"))));
+
+            if (parkingSpotId == null) {
+                path = "all-parking-spot-bills.json";
+                Block<Document> printBlock = new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+
+
+                        String parkingClientId = document.getString("parkingClientId");
+                        String parkingSpotId = document.getString("parkingSpotId");
+                        String vehicleId = document.getString("vehicleId");
+                        String billingRate = document.getString("billingRate");
+                        String action = document.getString("action");
+                        Date time = document.getDate("time");
+
+                        System.out.println(document.toJson());
+                    }
+                };
+                cursor.forEach(printBlock);
+            } else {
+                path = "parking-spot-bills.json";
+                Block<Document> printBlock = new Block<Document>() {
+                    @Override
+                    public void apply(final Document document) {
+
+
+                        String parkingClientId = document.getString("parkingClientId");
+                        String parkingSpotId = document.getString("parkingSpotId");
+                        String vehicleId = document.getString("vehicleId");
+                        String billingRate = document.getString("billingRate");
+                        String action = document.getString("action");
+                        Date time = document.getDate("time");
+
+                        System.out.println(document.toJson());
+                    }
+                };
+                cursor.forEach(printBlock);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
         }
 
-
+        path = "parking-spot-bills.json";
         byte[] sampleResponse = Files.readAllBytes(Paths.get(path));
         resp.setContentType("application/json");
         resp.getOutputStream().write(sampleResponse);
